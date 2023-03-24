600dffbea33fa47529f370e7065a985140a6ce64
johnny-gill <uint777@gmail.com>
2023-03-24 16:04:48 +0900

    엥이게왜안올라갔지

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+const ExpenseItem = (props) => {
+  console.log('ExpenseItem.js');
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    console.log(props.title);
+    setTitle('update');
+  }
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
