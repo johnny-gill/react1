005f05068ecddba4a1f1490c1b73af73a89a1e68
johnny-gill <uint777@gmail.com>
2023-03-27 23:37:18 +0900

    챠트 추가 마무으리~~~

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,20 @@
-import { useState } from "react";
-import "./Expenses.css";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import ExpensesList from "./ExpensesList";
+import { useState } from 'react';
+import './Expenses.css';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpenseChart from './ExpenseChart';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
 
   return (
     <div>
@@ -20,6 +23,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
           selected={filteredYear}
         />
+        <ExpenseChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
